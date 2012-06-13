783ea7c0d59e09d00a1e0f8716ec4d4856902355
Hai-Nam Nguyen <hainam@jcisio.com>
2012-06-13 09:25:03 +0200

    Task #1612370: Port MEE to D7 (cont.).

--- mee/mee.js
+++ mee/mee.js
@@ -1,69 +1,79 @@
 (function($) {
 
+Drupal.settings.mee = {
+  editors: {}
+};
+
 Drupal.behaviors.mee = {
   attach: function(context, settings) {
-    for (editor in Drupal.settings.dndDropAreas) {
-      var $editor = $('#' + editor);
-      $editor.bind('wysiwygAttach', Drupal.mee.attach);
-      $editor.bind('wysiwygDetach', Drupal.mee.detach);
+    for (editor in settings.dndDropAreas) {
+      $('#' + editor, context).each(function() {
+        var $this = $(this);
+        // I currently don't know how to effectively detect changes in
+        // textareas. So monitor theirs contents and periodically compare to the
+        // old values.
+        settings.mee.editors[$this.attr('id')] = '';
+        setInterval(function() {
+          Drupal.mee.update($this);
+        }, 1000);
+      });
     }
   }
 }
 
 Drupal.mee = {
-  attach: function(e, data) {
-    var t = setInterval(function() {
-      if (!tinyMCE) {
-        return;
-      }
-      var tiny_instance = tinyMCE.getInstanceById(data.field);
-      if (tiny_instance) {
-        clearInterval(t);
-        $(tiny_instance.editor_id)
-          .find('iframe')
-          .unbind('dnd_drop')
-          .bind('dnd_drop', function(e, data) {
-            var rep = Drupal.dnd.Atoms[data.representation_id];
-            if (!rep) return;
-            $(this)
-              .parents('div.mee-wrap-editor-library')
-              .find('table.mee-ressource-manager')
-              .each(function(i) {
-                var row = Drupal.mee.generate(
-                  data.representation_id,
-                  rep,
-                  Drupal.tableDrag[this.id]
-                );
-                if (row) {
-                  $(this).append(row);
-                }
-              });
-          });
-      }
-    }, 100);
-  },
-  detach: function(e, data) {
+  update: function(obj) {
+    var text = obj.val();
+    var id = obj.attr('id');
 
+    if (text === Drupal.settings.mee.editors[id]) {
+      return;
+    }
+
+    Drupal.settings.mee.editors[id] = text;
+
+    // 1. Check if there are known atoms.
+    // Known atoms are ones actually in the current library view and available
+    // for drag and drop. If library is unavailable, detection happens on the
+    // server side.
+    for (atom in Drupal.dnd.Atoms) {
+      // Theoretically we can search for Drupal.dnd.Atoms[atom].editor in the
+      // text, but we can, because RTE reformat the HTML source (eg. change
+      // class='image' into class="image" etc.).
+      if (text.indexOf('<!-- scald=' + atom + ':sdl_editor_representation -->') > -1) {
+        // @todo check the selector
+        Drupal.mee.generate(atom, obj.parents('.text-format-wrapper').find('.mee-resource-manager').attr('id'));
+      }
+    }
+
+    // @todo 2. Scan the resource manager table to clean up removed atoms.
   },
-  generate: function(id, representation, tableDrag) {
+
+ /**
+   * Generate a new item in the MEE Resource Manager table. For the sake of
+   * simplicity and performance, we don't use AHAH.
+   */
+  generate: function(atom_id, table_id) {
     var $weight = $("<select />"), $tr = $('<tr />'), $td = $("<td />"), parity;
-    var $separator = $(tableDrag.table).find('div.mee-rm-separator select');
+    var $tableDrag = $('#' + table_id);
+    var $separator = $tableDrag.find('div.mee-rm-separator select');
     var separator = $separator.get(0);
-    var wn = separator.name.replace(/\[0\]\[weight\]$/, '[' + id +'][weight]');
-    var action = representation.meta.action || 0;
+    var wn = separator.name.replace(/\[0\]\[weight\]$/, '[' + atom_id +'][weight]');
+    var action = Drupal.dnd.Atoms[atom_id].meta.action || 0;
     var $required = $("<select />")
       .attr('name', wn.replace(/\[weight\]$/, '[required]'))
       .append("<option value='0'>"+ Drupal.t('Optional') +"</option>")
       .append("<option value='1'>"+ Drupal.t('Required') +"</option>")
+      .addClass('form-select')
       .val(action);
+
     // If this ressource is already in the Ressource Manager, don't add a line
-    if ($('select[name="'+ wn +'"]', tableDrag.table).length) {
-      return '';
+    if ($('select[name="'+ wn +'"]', $tableDrag).length) {
+      return;
     }
     $tr
       .addClass('draggable')
-      .append($('<td></td>'))
-      .append($('<td></td>').append(representation.meta.title))
+      .append($('<td></td>').append(Drupal.dnd.Atoms[atom_id].meta.title))
       .append($('<td></td>').append($required));
     for (var i = -10; i <= 10; i++) {
       $weight.append("<option>"+ i +"</option>");
@@ -76,10 +86,10 @@ Drupal.mee = {
       .append($weight)
       .css('display', $separator.parents('td').css('display'));
     $tr.append($td);
-    parity = $(tableDrag.table).find('tr').size() % 2 ? 'odd' : 'even';
+    parity = $tableDrag.find('tr').size() % 2 ? 'odd' : 'even';
     $tr.addClass(parity);
-    tableDrag.makeDraggable($tr.get(0));
-    return $tr;
+    Drupal.tableDrag[table_id].makeDraggable($tr.get(0));
+    $tableDrag.find('tbody').append($tr);
   }
 }
 
