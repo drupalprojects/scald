6172466dec46398795d9c4e5d027dda8f5d5ffea
quadbyte <studio@quadbyte.net>
2012-06-20 11:59:23 -0400

    Scald Vimeo video provider.

---
+++ scald_vimeo/scald_vimeo_player.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ */
+?>
+<object width="<?php print $vars['video_width'] ?>" height="<?php print $vars['video_height'] ?>" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
+  <param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=<?php print $vars['video_id'] ?>&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1&amp;autoplay=0&amp;loop=0"></param>
+  <param name="allowFullScreen" value="true"></param>
+  <param name="allowScriptAccess" value="always"></param>
+  <param name="wmode" value="transparent"></param>
+  <object data="http://vimeo.com/moogaloop.swf?clip_id=<?php print $vars['video_id'] ?>&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" width="<?php print $vars['video_width'] ?>" height="<?php print $vars['video_height'] ?>" allowfullscreen="true" allowscriptaccess="always" wmode="transparent">
+    <img src="<?php print $vars['thumbnail'] ?>" alt="" class="dnd-dropped" width="<?php print $vars['video_width'] ?>" />
+  </object>
+</object>
