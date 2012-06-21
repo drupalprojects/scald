4f9ddb9df9e830189350c00fa2dd1316e395b828
quadbyte <studio@quadbyte.net>
2012-06-21 17:42:38 -0400

    SoundCloud audio provider.

---
+++ scald_soundcloud/scald_soundcloud_player.tpl.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ */
+?>
+<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http://api.soundcloud.com/tracks/<?php print $vars['track_id'] ?>&amp;show_artwork=true"></iframe>
