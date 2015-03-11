0f74749844acfb3cc15e79355fef9e1885c7cee4
nerocro <nerocro@1728164.no-reply.drupal.org>
2015-03-11 17:10:44 +0100

    Issue #2401515 by NerOcrO: Iframe title added for accessibility

--- modules/providers/scald_youtube/scald_youtube_player.tpl.php
+++ modules/providers/scald_youtube/scald_youtube_player.tpl.php
@@ -4,4 +4,4 @@
  *   Default theme implementation for the Scald Youtube Player
  */
 ?>
-<iframe width="<?php print $vars['video_width'] ?>" height="<?php print $vars['video_height'] ?>" src="//www.youtube.com/embed/<?php print $vars['video_id'] ?>" frameborder="0" allowfullscreen></iframe>
+<iframe title="<?php print $vars['title'] ?>" width="<?php print $vars['video_width'] ?>" height="<?php print $vars['video_height'] ?>" src="//www.youtube.com/embed/<?php print $vars['video_id'] ?>" frameborder="0" allowfullscreen></iframe>
