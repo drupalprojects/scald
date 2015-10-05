cff53adc21c65b557ec808780b5145ec77e37030
nagy.balint <nagy.balint@1763952.no-reply.drupal.org>
2015-10-05 11:14:16 +0200

    Issue #2578255 by nagy.balint, DeFr, prografr, gifad: Big performance problem on dnd_library when loading videos or audio

--- modules/providers/scald_audio/scald_audio_html5.tpl.php
+++ modules/providers/scald_audio/scald_audio_html5.tpl.php
@@ -5,7 +5,7 @@
  * Changed to use HTML5 Audio element by Mark Conroy - https://www.drupal.org/user/336910
  */
 ?>
-<audio controls>
+<audio controls preload="metadata">
  <source src="<?php echo file_create_url($vars['audio_uri']) ?>">
  <p>Your browser does not support the audio element.</p>
 </audio>
